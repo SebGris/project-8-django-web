44faf4c2933f8951d24ca6ee2d7b72ec75070def
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-05-27 16:26:14 +0200

    Ajout des modèles Band et Listing avec migrations, et mise à jour des vues pour afficher les données.

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,10 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
 
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>Mes groupes préférés sont :</p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+        </ul>
+        """)
 
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+
+def listings(request):
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>Mes groupes préférés sont :</p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+        """)
