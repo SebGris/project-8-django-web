95bfaad8b3da998307e7c4e46522212e3cb8854f
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-05-31 17:48:00 +0200

    Ajout des modèles Band et Listing avec leurs migrations, mise à jour des vues et des templates pour afficher les groupes et les détails des groupes.

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,29 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        ROCK = 'ROCK', 'Rock'
+        POP = 'POP', 'Pop'
+        JAZZ = 'JAZZ', 'Jazz'
+        CLASSICAL = 'CLASSICAL', 'Classical'
+        HIP_HOP = 'HIP_HOP', 'Hip Hop'
+        ELECTRONIC = 'ELECTRONIC', 'Electronic'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=20, default=Genre.ROCK)
+    biography = models.fields.CharField(max_length=1000, default='No biography provided')
+    year_formed = models.fields.IntegerField(
+        default=2021,
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
 
 class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+  
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,9 +4,16 @@ from listings.models import Band
 from listings.models import Listing
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', {'bands': bands})
+    return render(request, 'listings/band_list.html', {'bands': bands})
+
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request,
+                  'listings/band_detail.html',
+                   {'band' : band})
 
 
 def about(request):
